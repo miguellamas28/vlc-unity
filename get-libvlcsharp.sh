647a69060b09c9cc46c29778789035b7326bc504
Miguel Lamas <miguel.lamas@talentocorporativo.com>
2020-07-22 13:46:42 +0200

    fixes

--- get-libvlcsharp.sh
+++ get-libvlcsharp.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 
 # this script assumes there is the libvlcsharp repo next to vlc-unity
+dotnet msbuild /libvlcsharp/src/LibVLCSharp/LibVLCSharp.csproj /p:Unity=true,Configuration=Release
 
-rm -rf Assets/VLC-Unity-Windows/Plugins/x86_64/LibVLCSharp.dll
-
-msbuild ../libvlcsharp/src/LibVLCSharp/LibVLCSharp.csproj /p:Unity=true,Configuration=Release
-
-cp ../LibVLCSharp/src/LibVLCSharp/bin/Release/netstandard2.0/LibVLCSharp.dll Assets/VLC-Unity-Windows/Plugins/x86_64/
+mv /libvlcsharp/src/LibVLCSharp/bin/Release/netstandard2.0/LibVLCSharp.dll /Plugins/vlcsharp/LibVLCSharp.dll
